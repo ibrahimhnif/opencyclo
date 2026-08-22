2a91f772fbc55f4481456b64dc6be69620524cc1
Ibrahim hanif <ibrahim.hanif4529@gmail.com>
2026-08-22 14:48:13 +0700

    chore(ui): remove dead pre-rebuild page files superseded by the widget engine

--- src/ui/pages/climb_page.h
+++
@@ -1,9 +0,0 @@
-#ifndef OPENCYCLO_UI_PAGES_CLIMB_PAGE_H
-#define OPENCYCLO_UI_PAGES_CLIMB_PAGE_H
-
-#include "hardware/display.h"
-#include "core/telemetry_state.h"
-
-void renderClimbPage(const TelemetryState& state, bool forceFullRedraw);
-
-#endif // OPENCYCLO_UI_PAGES_CLIMB_PAGE_H
--- src/ui/pages/debug_page.h
+++
@@ -1,9 +0,0 @@
-#ifndef OPENCYCLO_UI_PAGES_DEBUG_PAGE_H
-#define OPENCYCLO_UI_PAGES_DEBUG_PAGE_H
-
-#include "hardware/display.h"
-#include "core/telemetry_state.h"
-
-void renderDebugPage(const TelemetryState& state, bool forceFullRedraw);
-
-#endif // OPENCYCLO_UI_PAGES_DEBUG_PAGE_H
